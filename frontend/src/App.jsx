b0a79d6b519b3d82c6814aa67549e294d733f627
Chloe-Sirgent <chloe.sirgent@gmail.com>
2022-06-30 17:16:20 +0200

    correction

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,8 @@
 /* eslint-disable react/jsx-no-constructed-context-values */
 import { Routes, Route, BrowserRouter as Router } from "react-router-dom";
-import React, { useState } from "react";
+import React from "react";
 import Login from "./pages/Login";
 import ProjectInfo from "./pages/ProjectInfo";
-import UserIdContext from "./components/UserIdContext";
 import AddProject from "./pages/AddProject";
 import Home from "./pages/Home";
 import ProjectsList from "./components/ProjectsList";
@@ -15,16 +14,14 @@ function App() {
   return (
     // eslint-disable-next-line react/jsx-no-comment-textnodes
     <Router>
-      <UserIdContext.Provider value={{ userId, setUserId }}>
-        <Routes>
-          <Route path="/" element={<Login />} />
-          <Route path="/projectinfo" element={<ProjectInfo />} />
-          <Route path="/adduser" element={<AddUser />} />
-          <Route path="/projectslist" element={<ProjectsList />} />
-          <Route path="/home" element={<Home />} />
-          <Route path="/add-project" element={<AddProject />} />
-        </Routes>
-      </UserIdContext.Provider>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/projectinfo" element={<ProjectInfo />} />
+        <Route path="/adduser" element={<AddUser />} />
+        <Route path="/projectslist" element={<ProjectsList />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/add-project" element={<AddProject />} />
+      </Routes>
     </Router>
   );
 }
