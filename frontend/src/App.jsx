4d3808fb20b495719ab9f22217591012ac7aa28d
Alexis Olivier <alexis-olivier_student2022@wilder.school>
2022-06-30 09:56:46 +0200

    merge dev

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,49 +1,32 @@
 /* eslint-disable react/jsx-no-constructed-context-values */
-import { BrowserRouter as Router, Route, Link, Routes } from "react-router-dom";
-import React, { useState } from "react";
-import LoginMember from "./pages/LoginMember";
+// import { BrowserRouter as Router, Route, Link, Routes } from "react-router-dom";
+// import React, { useState } from "react";
+// import LoginMember from "./pages/LoginMember";
 // import Users from "./pages/Users";
 // import Logout from "./pages/Logout";
 // import EspaceMember from "./pages/EspaceMember";
-import UserIdContext from "./components/UserIdContext";
-import Body from "./pages/Body";
-import "./App.css";
+// import UserIdContext from "./components/UserIdContext";
+// import Body from "./pages/Body";
+// import "./App.css";
+import { Routes, Route, BrowserRouter as Router } from "react-router-dom";
+import Login from "./pages/Login";
+import ProjetInfo from "./pages/ProjetInfo";
+import AddProject from "./pages/AddProject";
+import Home from "./pages/Home";
+import ProjectsList from "./components/ProjectsList";
 
 function App() {
-  const [userId, setUserId] = useState("");
+  // const [userId, setUserId] = useState("");
   return (
-    <>
-      <Body />
-      <Router>
-        <div className="app">
-          <nav>
-            <ul
-              style={{
-                display: "flex",
-                justifyContent: "space-around",
-                fontSize: "30px",
-              }}
-            >
-              <li>
-                <Link to="/loginMember">Membre</Link>
-              </li>
-              <li>
-                <Link to="/loginAdmin">Admin </Link>
-              </li>
-              <li>
-                <Link to="/logout">Disconnect</Link>
-              </li>
-            </ul>
-          </nav>
-          <UserIdContext.Provider value={{ userId, setUserId }}>
-            <Routes>
-              <Route path="/loginMember" element={<LoginMember />} />
-              {/* <Route path="/espacemember" element={<EspaceMember />} /> */}
-            </Routes>
-          </UserIdContext.Provider>
-        </div>
-      </Router>
-    </>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/projectinfo" element={<ProjetInfo />} />
+        <Route path="/projectslist" element={<ProjectsList />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/add-project" element={<AddProject />} />
+      </Routes>
+    </Router>
   );
 }
 export default App;
